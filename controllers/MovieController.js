bed926d06b7e0c3376a0e3f54ec54168178bb4e0
XNycX <nycx2009@hotmail.com>
2022-02-17 16:22:46 +0100

    MovieController functions added for DB

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -6,96 +6,155 @@ const axios = require('axios');
     
 //Funciones del controlador
 
-MovieController.getPlayingMovie = async (req, res) => {
-        try {
-            let result = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
-            res.send(result.data);
-        } catch (error) {
-            res.send(error);
-        };
+// MovieController.getPlayingMovie = async (req, res) => {
+//         try {
+//             let result = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
+//             res.send(result.data);
+//         } catch (error) {
+//             res.send(error);
+//         };
+// };
+
+// MovieController.getPopularMovie = async (req, res) => {
+//     try {
+//         let result = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US");
+//         res.send(result.data)
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getTopMovie = async (req, res) => {
+    
+//     try {
+//         let result = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getTitleMovie = async (req, res) => {
+//     let search = req.query.id
+//     try {
+//         let result = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${search}&page=1&include_adult=false`);
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getNews = async (req, res) => {
+
+//     try {
+
+//         let result = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
+
+//         res.send(result.data);
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// MovieController.getMovieById = async (req, res) => {
+//     let search = req.query.id
+//     try {
+//         let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getReviewById = async (req, res) => {
+//     let search = req.query.id
+//     try {
+//         let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}/reviews?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1`);
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getLastestMovie = async (req, res) => {
+//     try {
+//         let result = await axios.get(`https://api.themoviedb.org/3/movie/latest?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+
+// MovieController.getSimilarMovieById = async (req, res) => {
+//     let search = req.query.id
+//     try {
+//         let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}/similar?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1`);
+//         res.send(result.data);
+//     } catch (error) {
+//         res.send(error);
+//     };
+// };
+MovieController.getMovie = (req, res) => {
+    //Búsqueda trayendo a todos los Movies
+    Movie.findAll()
+    .then(data => {
+
+        res.send(data)
+    });
+
 };
 
-MovieController.getPopularMovie = async (req, res) => {
-    try {
-        let result = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US");
-        res.send(result.data)
-    } catch (error) {
-        res.send(error);
-    };
+MovieController.getMovieById = (req, res) => {
+    //Búsqueda buscando una Id
+    Movie.findByPk(req.params.id)
+    .then(data => {
+        res.send(data)
+    });
 };
 
-MovieController.getTopMovie = async (req, res) => {
+MovieController.getMovieByEmail = (req, res) => {
+    //Búsqueda comparando un campo
+    Movie.findOne({ where : { email : req.params.email }})
+    .then(data => {
+        res.send(data)
+    });
+}
+
+MovieController.postMovie = async (req, res) => {
+
+    //Registrando un Movie
     
     try {
-        let result = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
-        res.send(result.data);
+
+        let title = req.body.title;
+        let overview = req.body.overview;
+        let adult = req.body.adult;
+        let popularity = req.body.popularity;
+        let img = req.body.img;
+        let release_date = req.body.release_date;
+
+        //Comprobación de errores.....
+
+        //Guardamos en sequelize el Movie
+
+        Movie.create({
+            title: title,
+            overview: overview,
+            adult: adult,
+            popularity: popularity,
+            img: img,
+            release_date: release_date
+        }).then(Movie => {
+            console.log("esta es la pelicula", Movie);
+            res.send(`${usuario.name}, se ha añadido correctamente`);
+        });
+
     } catch (error) {
         res.send(error);
-    };
-};
-
-MovieController.getTitleMovie = async (req, res) => {
-    let search = req.query.id
-    try {
-        let result = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&query=${search}&page=1&include_adult=false`);
-        res.send(result.data);
-    } catch (error) {
-        res.send(error);
-    };
-};
-
-MovieController.getNews = async (req, res) => {
-
-    try {
-
-        let result = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1");
-
-        res.send(result.data);
-
-    } catch (error) {
-        console.log(error);
     }
+    
 };
-
-MovieController.getMovieById = async (req, res) => {
-    let search = req.query.id
-    try {
-        let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
-        res.send(result.data);
-    } catch (error) {
-        res.send(error);
-    };
-};
-
-MovieController.getReviewById = async (req, res) => {
-    let search = req.query.id
-    try {
-        let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}/reviews?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1`);
-        res.send(result.data);
-    } catch (error) {
-        res.send(error);
-    };
-};
-
-MovieController.getLastestMovie = async (req, res) => {
-    try {
-        let result = await axios.get(`https://api.themoviedb.org/3/movie/latest?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
-        res.send(result.data);
-    } catch (error) {
-        res.send(error);
-    };
-};
-
-MovieController.getSimilarMovieById = async (req, res) => {
-    let search = req.query.id
-    try {
-        let result = await axios.get(`https://api.themoviedb.org/3/movie/${search}/similar?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1`);
-        res.send(result.data);
-    } catch (error) {
-        res.send(error);
-    };
-};
-
 
 // MovieController.OpinionMovie = (req, res) => {
 
--- routes/movies.js
+++ routes/movies.js
@@ -3,44 +3,59 @@ const MovieController = require('../controllers/MovieController');
 const router = express.Router();
 //CRUD RESTFUL
 
-//Mostrar peliculas actualmente en cines
-router.get('/playing', MovieController.getPlayingMovie);
-//http://localhost:3000/movies/playing
 
-//Registrar opinion
-// router.post('/', MovieController.registerOpinion);
-//http://localhost:3000/movies
+// //Mostrar usuarios por ID
+//Leer todos los usuarios
+router.get('/', MovieController.getMovie);
+//http://localhost:3000/usuarios
 
-//Mostrar peliculas populares
-router.get('/popular', MovieController.getPopularMovie);
-//http://localhost:3000/movies/popular
+router.get('/:id', MovieController.getMovieById);
 
-//Mostrar peliculas mejor valoradas
-router.get('/top', MovieController.getTopMovie);
-//http://localhost:3000/movies/top
+router.get('/email/:email', MovieController.getMovieByEmail);
 
-//Mostrar peliculas por ID
-router.get('/', MovieController.getMovieById);
-//http://localhost:3000/movies
+//Registro
+router.post('/', MovieController.postMovie);
 
-//Mostrar reviews por ID
-router.get('/reviews', MovieController.getReviewById);
-//http://localhost:3000/movies/reviews
 
-//Mostrar las ultimas peliculas
-router.get('/lastest', MovieController.getLastestMovie);
-//http://localhost:3000/movies/lastest
 
-//Mostrar peliculas similares a la pelicula mostrada por ID
-router.get('/similar', MovieController.getSimilarMovieById);
-//http://localhost:3000/movies/similar
+// //Mostrar peliculas actualmente en cines
+// router.get('/playing', MovieController.getPlayingMovie);
+// //http://localhost:3000/movies/playing
 
-//Borrar opinion
-// router.delete('/', MovieController.deleteOpinion);
-//https://localhost:3000/movies/
+// //Registrar opinion
+// // router.post('/', MovieController.registerOpinion);
+// //http://localhost:3000/movies
 
-router.get('/title', MovieController.getTitleMovie)
+// //Mostrar peliculas populares
+// router.get('/popular', MovieController.getPopularMovie);
+// //http://localhost:3000/movies/popular
 
-router.get('/news', MovieController.getNews);
+// //Mostrar peliculas mejor valoradas
+// router.get('/top', MovieController.getTopMovie);
+// //http://localhost:3000/movies/top
+
+// //Mostrar peliculas por ID
+// router.get('/', MovieController.getMovieById);
+// //http://localhost:3000/movies
+
+// //Mostrar reviews por ID
+// router.get('/reviews', MovieController.getReviewById);
+// //http://localhost:3000/movies/reviews
+
+// //Mostrar las ultimas peliculas
+// router.get('/lastest', MovieController.getLastestMovie);
+// //http://localhost:3000/movies/lastest
+
+// //Mostrar peliculas similares a la pelicula mostrada por ID
+// router.get('/similar', MovieController.getSimilarMovieById);
+// //http://localhost:3000/movies/similar
+
+// //Borrar opinion
+// // router.delete('/', MovieController.deleteOpinion);
+// //https://localhost:3000/movies/
+
+// router.get('/title', MovieController.getTitleMovie)
+
+// router.get('/news', MovieController.getNews);
 
 module.exports = router;
