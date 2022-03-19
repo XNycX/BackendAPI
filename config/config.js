9d63098c412735e044159464e9782a719d428bb1
“Cristian” <nycx2009@hotmail.com>
2022-03-19 15:30:01 +0100

    add env variable cleardb url

--- config/config.js
+++ config/config.js
@@ -20,6 +20,7 @@ module.exports= {
       "password": process.env.MYSQL_PASSWORD,
       "database": process.env.MYSQL_DATABASE,
       "host": process.env.MYSQL_HOST,
+      "use_env_variable":"CLEARDB_DATABASE_URL",
       "dialect":"mysql"
     }
   }
