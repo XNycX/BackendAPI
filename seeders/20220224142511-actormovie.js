61357dec9a32970fe2421af0de1c52587e1854f4
“Cristian” <nycx2009@hotmail.com>
2022-03-19 16:11:44 +0100

    fix naming convention on actormovie seeder

--- seeders/20220224142511-actormovie.js
+++ seeders/20220224142511-actormovie.js
@@ -4,13 +4,13 @@ module.exports = {
   async up (queryInterface, Sequelize) {
 
     await queryInterface.bulkInsert('actormovies', [
-    {"actorId":1,"movieId":1,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":3,"movieId":1,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":2,"movieId":2,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":4,"movieId":3,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":5,"movieId":4,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":6,"movieId":5,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
-    {"actorId":7,"movieId":5,"createdAt":"2021-02-24","updatedAt":"2021-02-26"}
+    {"ActorId":1,"MovieId":1,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":3,"MovieId":1,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":2,"MovieId":2,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":4,"MovieId":3,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":5,"MovieId":4,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":6,"MovieId":5,"createdAt":"2021-02-24","updatedAt":"2021-02-26"},
+    {"ActorId":7,"MovieId":5,"createdAt":"2021-02-24","updatedAt":"2021-02-26"}
      
       ], {});
     
